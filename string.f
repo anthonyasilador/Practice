7ea389b301b9cea415ec36603569210142e943da
anthonyasilador <67173106+anthonyasilador@users.noreply.github.com>
2020-06-19 19:13:43 -0400

    Add files via upload

---
+++ string.f
@@ -0,0 +1,31 @@
+        program string
+        real y(1000,3)
+        x0=0.3
+        print*,"Enter the number of time steps"
+        read(5,*) nts
+        open(7,file="wave")
+  110   format(2(2x,e12.5))
+        do i=1,1000
+          x=0.001*i
+          y(i,1)=exp(-1000.0*(x-x0)**2)
+          y(i,2)=y(i,1)
+        enddo
+        do ii=1,nts ! time steps
+          do jt=2,3
+            y(1,jt)=0.0
+            y(1000,jt)=0.0
+           enddo
+          do i=2,999
+            y(i,3)=y(i+1,2)+y(i-1,2)-y(i,jt-1)
+          enddo
+          do i=2,999
+           do jt=2,3
+             y(i,jt-1)=y(i,jt)
+           enddo
+          enddo
+        enddo ! time steps
+        do i=1,1000
+          x=0.001*i
+          write(7,110) x,y(i,3)
+        enddo
+        end
